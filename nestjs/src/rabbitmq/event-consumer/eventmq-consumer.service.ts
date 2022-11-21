31bdfb51dba1031cd31084ab3d43a391db0b146e
Quan Tran <quantran@wakumo.vn>
2022-11-21 16:43:00 +0700

    add rabbitmq rpc message handler

--- nestjs/src/rabbitmq/event-consumer/eventmq-consumer.service.ts
+++ nestjs/src/rabbitmq/event-consumer/eventmq-consumer.service.ts
@@ -1,7 +1,11 @@
-import { RabbitSubscribe } from "@golevelup/nestjs-rabbitmq";
+import { RabbitRPC, RabbitSubscribe } from "@golevelup/nestjs-rabbitmq";
 import { Injectable } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
 import { ConsumeMessage, Channel } from 'amqplib'
+
+function errorHandler(channel: Channel, msg: ConsumeMessage, error: Error) {
+  console.log(error);
+  channel.reject(msg, false);
+}
 
 @Injectable()
 export class EventMqConsumerService {
@@ -11,14 +15,22 @@ export class EventMqConsumerService {
     exchange: "sample_exchange",
     routingKey: "sample.routing_key.*",
     queue: "sample_queue",
-    errorHandler: (channel: Channel, msg: ConsumeMessage, error: Error) => {
-      console.log(error);
-      channel.reject(msg, false);
-    }
+    errorHandler
   })
-  public async getSampleMessage(msg: {}, amqpMsg: ConsumeMessage) {
-    console.log("go here");
-    const data = JSON.parse(amqpMsg.content.toString())
-    console.log(data);
+  public async getSampleMessage(msg: string, amqpMsg: ConsumeMessage) {
+    const routingKey = amqpMsg.fields.routingKey;
+    console.log(`Received message at ${routingKey}: ${msg}`);
+  }
+
+  @RabbitRPC({
+    exchange: "sample_exchange",
+    routingKey: "sample.routing_key_rpc.*",
+    queue: "sample_queue_rpc",
+    errorHandler
+  })
+  public async getSampleRpcMessage(msg: string, amqpMsg: ConsumeMessage) {
+    const routingKey = amqpMsg.fields.routingKey;
+    console.log(`Received message at ${routingKey}: ${msg}`);
+    return true;
   }
 }
