820b29ae22ce01d6a1fcc4fba4b1878c67e3d1b4
quantbwkm <quantran@wakumo.vn>
2023-10-03 17:07:48 +0700

    add port for pg_client

--- nestjs/pg_client.ts
+++ nestjs/pg_client.ts
@@ -9,6 +9,7 @@ export const dbNameTest = process.env.DB_NAME_TEST;
 export const client = new Client({
   user: process.env.DB_USER || 'root',
   host: process.env.DB_HOST || 'local_postgres_user',
+  port: Number(process.env.DB_PORT),
   database: 'postgres',
   password: process.env.DB_PASSWORD || '',
 });
